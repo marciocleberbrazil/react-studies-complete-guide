9db480e0258938452020e7e13cdf48b9fddd2e76
Marcio Barboza <marciocleberbrazil@gmail.com>
2020-04-11 15:36:59 +1000

    Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
-import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <Auxiliary>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.change} defaultValue={this.props.name} />
-            </Auxiliary>
+            </Fragment>
         )
     }
 };
